3927f80eba2511e1c75dffa71d821bd2408c0c5c
Felipe Devilla <felipe.devilla@gmail.com>
2023-09-11 20:45:56 -0300

    Feito exercicio 20

--- ListaDeExercicio1/20).js
+++ ListaDeExercicio1/20).js
@@ -1,11 +1,19 @@
 function informValue(total) {
-    let pegarQntd
-    let resto //usar let resto para coiferir otras nootas
+    const notas = [100, 50, 20, 10, 5, 1]
+    let quantidadeNotas = Array(notas.length).fill(0)
 
-   if(total >= 100){
-    pegarQntd = total /100
-    console.log(`Vai precisar de ${pegarQntd} notas de 100`)
-   } 
+    for (let i = 0; i < notas.length; i++) {
+        while (total >= notas[i]) {
+            total -= notas[i];
+            quantidadeNotas[i]++;
+        }
+    }
+
+    for (let i = 0; i < notas.length; i++) {
+        if (quantidadeNotas[i] > 0) {
+            console.log(`Vai precisar de ${quantidadeNotas[i]} nota(s) de R$${notas[i]}`);
+        }
+    }
 }
 
-informValue(500)
+informValue(570)
