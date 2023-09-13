403f5efd946eb860ebbd7942985145fb02e0974d
Felipe Devilla <felipe.devilla@gmail.com>
2023-09-13 06:53:29 -0300

    Terminado o 21

--- ListaDeExercicio1/21).js
+++ ListaDeExercicio1/21).js
@@ -1,6 +1,16 @@
-function valuePlano(idade){
+function valuePlano(idadeConveniado) {
     const valorPadrao = 100
+    const idades = [10, 30, 60, 61]
     const adcionais = [80, 50, 95, 130]
+    let valorTotal
 
-    
+
+    for (let i = 0; i < idades.length & valorTotal == undefined; i++) {
+        if (idades[i] >= idadeConveniado) {
+            valorTotal = valorPadrao + adcionais[i]
+        }
+    }
+    console.log(valorTotal)
 }
+
+valuePlano(43)
