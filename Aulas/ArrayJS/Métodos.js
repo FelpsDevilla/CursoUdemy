6dc61067810805b15bb377f8a143af26a53dfc80
Felipe Rossetti Devilla <felipe.devilla@ccgl.intra>
2023-10-05 15:45:18 -0300

    Iniciado Arrays

---
+++ Aulas/ArrayJS/Métodos.js
@@ -0,0 +1,15 @@
+const heroisDC = ["Batman", "Superman", "Besouro Azul", "Flash"];
+heroisDC.pop(); //Remove o ultimo elemento do array
+console.log(heroisDC);
+
+heroisDC.push("Lanterna Verde"); //Adciona no ultimo elemento
+console.log(heroisDC);
+
+heroisDC.shift() //Remove o primeiro elemento
+console.log(heroisDC)
+
+heroisDC.unshift("SuperChoque") //Adciona no primeiro elemento
+console.log(heroisDC)
+
+//Splice pode tanto adcionar como remover elementos
+
